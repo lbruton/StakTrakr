ae9c1bcb6e4ed02016d5eee70387cfcb74e37e47
lbruton <lbruton@lonniebruton.com>
2026-05-13 21:53:17 -0500

    feat(STRK-75): vendor column union for all-scope table

--- js/market-data.js
+++ js/market-data.js
@@ -986,10 +986,24 @@ const _renderVendorTable = async (metalCode) => {
   }
 
   const allVendorIds = new Set();
-  for (const slug in detailMap) {
-    const vendors = detailMap[slug].vendors;
-    if (vendors) {
-      for (const vid in vendors) allVendorIds.add(vid);
+  if (isAllScope) {
+    for (const { slug } of metalSlugs) {
+      const detail = detailMap[slug];
+      const vendors = detail && detail.vendors;
+      if (!vendors) continue;
+      for (const vid in vendors) {
+        if (typeof _isMarketItemEnabled === "function" && !_isMarketItemEnabled(slug, vid)) {
+          continue;
+        }
+        allVendorIds.add(vid);
+      }
+    }
+  } else {
+    for (const slug in detailMap) {
+      const vendors = detailMap[slug].vendors;
+      if (vendors) {
+        for (const vid in vendors) allVendorIds.add(vid);
+      }
     }
   }
 
